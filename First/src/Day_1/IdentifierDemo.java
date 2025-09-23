8f742535d177b9fd043ba9476b9058220553767e
Suganthan <Suganthan@DESKTOP-NB1HIRS>
2025-09-23 21:30:29 +0530

    completed  day1

---
+++ First/src/Day_1/IdentifierDemo.java
@@ -0,0 +1,15 @@
+package Day_1;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("value of the number is :"+$number);
+		
+		String studentName="suganthan";
+		System.out.println("value is:"+studentName);
+		
+
+	}
+
+}
--- First/src/program/java_one.java
+++
@@ -1,8 +0,0 @@
-package program;
-
-public class java_one {
-	public static void main(String[] args) {
-		System.out.print("hello world");
-	}
-
-}
