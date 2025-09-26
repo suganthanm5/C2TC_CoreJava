feef16c804f8df8bf017e22399796cb42fcacc7b
= <=>
2025-09-26 21:01:01 +0530

    day 2 completed

---
+++ C2TC_CoreJava/src/Day_2/DoWhileDem.java
@@ -0,0 +1,15 @@
+package Day_2;
+
+public class DoWhileDem {
+
+	public static void main(String[] args) {
+		int i = 11;
+		do {
+			System.out.println(i);
+			
+		}while(i <=1);
+		
+
+	}
+
+}
--- C2TC_CoreJava/src/Day_2/Objects.java
+++
@@ -1,5 +0,0 @@
-package Day_2;
-
-public class Objects {
-
-}
