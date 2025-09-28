2322bb84fbd2cb6ede4256299c559a9746dc16bc
= <=>
2025-09-28 11:14:06 +0530

    day 3 completed

---
+++ C2TC_CoreJava/src/Day_3/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package Day_3;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+
+	}
+
+}
