4619b46a4e126692ffb3b802580a274f2ac9663f
= <=>
2025-09-29 20:20:45 +0530

    day 4 completed

---
+++ C2TC_CoreJava/src/Day_4/secondpackage/Executor.java
@@ -0,0 +1,21 @@
+package Day_4.secondpackage;
+
+import Day_4.firstpackage.Base;
+
+public class Executor {
+	public static void main(String[] args) {
+		
+		Base b1 = new Base();
+
+		
+		 
+
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+	}
+}
+
+
+
+
